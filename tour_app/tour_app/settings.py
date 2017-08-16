429f6183d07761b8042755e3d64ea47ff8802f5e
Daniel Hernandez <dan@MBP-de-Daniel.home>
2017-08-15 23:50:40 -0300

    change setting

--- tour_app/tour_app/settings.py
+++ tour_app/tour_app/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'tour_app',
         'USER': 'tour_app',
-        'PASSWORD': '621996Dan',
+        'PASSWORD': 'tou_4pp',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
